29d0558b83554708443bf38b30fd19a01e6cc04b
Manish <manishch01@gmail.com>
2025-03-18 20:37:18 +0530

    Update code for Gallery

---
+++ Models/ImageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hydrox.Models
+{
+    public class ImageModel
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string Category { get; set; }
+    }
+}
